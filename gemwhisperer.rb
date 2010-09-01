6a05097774deaf66d4e6a5209d0b822a1562393a
Nick Quaranto <nick@quaran.to>
2010-08-31 22:22:21 -0400

    attempt to use twitter oauth to post updates

--- gemwhisperer.rb
+++ gemwhisperer.rb
@@ -2,6 +2,11 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'haml'
 require 'json'
+require 'twitter'
+
+oauth  = Twitter::OAuth.new(ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'])
+oauth.authorize_from_access(ENV['REQUEST_TOKEN'], ENV['REQUEST_SECRET'])
+client = Twitter::Base.new(oauth)
 
 configure :development do
   ActiveRecord::Base.establish_connection(:adapter  => "sqlite3",
@@ -23,11 +28,13 @@ end
 
 post '/' do
   begin
-    hash = JSON.parse(request.body.read)
-    Whisper.create(:name    => hash["name"],
-                   :version => hash["version"],
-                   :url     => hash["project_uri"],
-                   :info    => hash["info"])
+    hash    = JSON.parse(request.body.read)
+    whisper = Whisper.create(:name    => hash["name"],
+                             :version => hash["version"],
+                             :url     => hash["project_uri"],
+                             :info    => hash["info"])
+
+    client.update("#{whisper.name} (#{whisper.version}): #{whisper.url}")
   rescue Exception => ex
     # oops!
   end
